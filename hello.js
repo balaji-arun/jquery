247c30474a41d04ff72534eb8f4451d77cdcddff
balaji-arun <53910355+balaji-arun@users.noreply.github.com>
2019-09-19 21:05:33 +0530

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    /* Retrieving an element's text */
+
+    //You can find any DOM element and it's text using this syntax:
+    // text() is a method offered by jQuery
+    $("h1").text(); //returns "jQuery Basics"
+
+    //text() allows you to modify the text node
+    $("h1").text("jQuery: Basics")
+
+});
